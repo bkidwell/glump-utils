1c93bca5ebde7b77146374dad31344523561c396
Brendan Kidwell <Brendan_Kidwell@abtassoc.com>
2016-08-16 19:43:34 -0400

    QR code encoder: us phone number

--- qr/qrweb.js
+++ qr/qrweb.js
@@ -6,6 +6,25 @@ $('#go').click(function() {
     var data = $('#data').val();
     var match = null;
 
+    if($('input[name=mode]:checked').val() == 'phone') {
+        var meeting = null;
+        var phone = null;
+        data = ' ' + data + ' ';
+        match = data.match(/[^\d\-]\+?(1[\- \(\)]*)?(\d\d\d[\- \(\)]*\d\d\d[\- \(\)]*\d\d\d\d)[^\d\-]/);
+        if(match && match[2]) {
+            console.log('phone match: ' + match[1]);
+            phone = match[2].replace(/[ \-\(\)]+/g, '');
+            phone = '+1-' + phone.substr(0, 3) + '-' + phone.substr(3, 3) + '-' + phone.substr(6, 4);
+            console.log('phone: ' + phone);
+        }
+
+        if(phone) {
+            data = 'tel:' + phone
+        } else {
+            data = '(error)';
+        }
+    }
+
     if($('input[name=mode]:checked').val() == 'gtm') {
         var meeting = null;
         var phone = null;
